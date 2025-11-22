0b9e47567bfcdaae1c0bc25842f3480c41856090
snowvajohn <snowvajohn@gmail.com>
2025-11-22 20:20:45 +0700

    removed comment

--- src/components/MainContent.test.jsx
+++ src/components/MainContent.test.jsx
@@ -5,7 +5,6 @@ import userEvent from '@testing-library/user-event';
 import MainContent from './MainContent';
 
 // My Tests
-// Add yet another comment
 
 describe('MainContent', () => {
   it('should render a button', () => {
